2db2ec8ecc0012835d8612a8500231364a196a70
unknown <mattemendu@gmail.com>
2019-06-25 18:36:04 +0200

    commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require('express');
+const cors = require('cors');
+
+app.use(cors());
+const server = require('http').createServer(app).listen(3000);
+
+const io = require('socket.io').listen(server);
+
+io.set('transports', [ 'websocket' ] );
+
+io.on('connection', function (socket) {
+		console.log('new connection');
+		socket.broadcast.emit('message', {topic:'new connection', message:'user'});
+});
